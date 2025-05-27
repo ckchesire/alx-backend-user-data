21030ce0844edac1c70d0b8b60fe29b48234e249
Christian Chesire <christianck.chesire@gmail.com>
2025-05-27 16:38:05 +0300

    Added method to extract base64 authorization header, which returns the Base64 part of the Authorization header for Basic Authentication.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,25 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """Implements Basic Auth
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        Extracts Base64 part of the Authorization header for Basic
+        Authentication.
+
+        Args:
+            authorization_header (str): Value of the Authorization header.
+
+        Returns:
+            str: returns the Base64 part of the header, or None if failed.
+        """
+        if authorization_header is None:
+            return None
+
+        if not isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        return authorization_header[len("Basic "):]
