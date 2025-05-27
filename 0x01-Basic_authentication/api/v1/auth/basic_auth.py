1a0d9bbf46d2b6d7900e7f89f9b97e548e2ae6d4
Christian Chesire <christianck.chesire@gmail.com>
2025-05-27 15:34:32 +0300

    Created BasicAuth class that inherits from Auth.

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module that implements Basic Authentication scheme.
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Implements Basic Auth
+    """
+    pass
