43a8e05f031ba532d862ec07b34812f96099a089
Christian Chesire <christianck.chesire@gmail.com>
2025-05-27 17:50:24 +0300

    Implemented method to decode base64 authorization header, and return the decoded value of a Base64 string.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """Module that implements Basic Authentication scheme.
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -29,3 +30,28 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[len("Basic "):]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """
+        Decodes the value of a Base64 string.
+
+        Args:
+            base64_authorization_header: base64 encoded string to be decoded
+
+        Returns:
+            str: returns the decoded UTF-8 string value of a Base64 string, or
+            None on failure.
+        """
+        if base64_authorization_header is None:
+            return None
+
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_auth = base64.b64decode(base64_authorization_header)
+            return decoded_auth.decode('utf-8')
+        except Exception:
+            return None
