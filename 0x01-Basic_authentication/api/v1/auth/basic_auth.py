f95deed03794750cf09635e80567c437b529fe38
Christian Chesire <christianck.chesire@gmail.com>
2025-05-28 01:24:28 +0300

    Added method to create user object from credentials, that returns the User instance based on their email and password.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 """
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -82,3 +84,37 @@ class BasicAuth(Auth):
 
         email, password = decode_base64_authorization_header.split(':', 1)
         return email, password
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """
+        Method to return User instance based on email and password.
+
+        Args:
+           user_email (str): a particular user email
+           user_pwd (str): a particular user password
+
+        Return:
+            User: returns the User instance based on their email and password.
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        try:
+            users = User.search({"email": user_email})
+        except Exception:
+            return None
+
+        if not users or len(users) == 0:
+            return None
+
+        user = users[0]
+
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
