1cbeb852b1eb7e999f6e15992a4173387feb40bb
Christian Chesire <christianck.chesire@gmail.com>
2025-05-29 16:45:00 +0300

    Added instance method that returns a user id based on the session id, in the class session authentication.

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -13,7 +13,7 @@ class SessionAuth(Auth):
     def create_session(self, user_id: str = None) -> str:
         """
         Method that creates a Session ID for a given user_id. And stores the
-        Session ID and
+        Session ID in the class dictionary.
 
         Args:
            user_id (str): a particular user's id.
@@ -31,3 +31,22 @@ class SessionAuth(Auth):
         self.__class__.user_id_by_session_id[session_id] = user_id
 
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Method returns a user_id based on session id.
+
+        Args:
+           session_id (str): a particular session id.
+
+        Returns:
+            strs: returns a user id if successfull, else None.
+        """
+        if session_id is None:
+            return None
+
+        if not isinstance(session_id, str):
+            return None
+
+        user_id = self.__class__.user_id_by_session_id.get(session_id)
+        return user_id
