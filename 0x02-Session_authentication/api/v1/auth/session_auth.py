748a25f35a091c8d5336e926f771badb801b9899
Christian Chesire <christianck.chesire@gmail.com>
2025-05-29 15:39:47 +0300

    Updated Session Authentication classthat creates a Session ID for a given user_id. And stores the Session ID in the class dictionary.

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,9 +2,32 @@
 """Module to perfom session authentication.
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """Implements session authentication.
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Method that creates a Session ID for a given user_id. And stores the
+        Session ID and
+
+        Args:
+           user_id (str): a particular user's id.
+
+        Returns:
+            str: returns the session id string if successful otherwise None.
+        """
+        if user_id is None:
+            return None
+
+        if not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.__class__.user_id_by_session_id[session_id] = user_id
+
+        return session_id
