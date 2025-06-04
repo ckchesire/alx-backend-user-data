09fc8e7c2580a6c9db10be8bd3a7ada266d4b777
Christian Chesire <christianck.chesire@gmail.com>
2025-06-05 00:12:59 +0300

    Updated arguments for add_user method to be inline with requirements.

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -29,10 +29,10 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, pwd: str) -> User:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Method to create a new user in the database.
         """
-        user = User(email=email, hashed_password=pwd)
+        user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         return user
