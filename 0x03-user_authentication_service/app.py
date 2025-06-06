9f3dcd373c87c43d74f0ebbab65715962b2a9bb3
Christian Chesire <christianck.chesire@gmail.com>
2025-06-06 16:51:54 +0300

    Implemented a get_reset_password_token function to respond to the POST /reset_password route.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -87,5 +87,22 @@ def profile():
     return jsonify({"email": user.email}), 200
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token():
+    """
+    POST /reset_password endpoint to get a reset token
+    """
+    email = request.form.get("email")
+    if email is None:
+        abort(403)
+
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+
+    return jsonify({"email": email, "reset_token": reset_token}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
