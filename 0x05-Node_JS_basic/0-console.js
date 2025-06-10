14b34beeb73e59d148ebf53e9143dc2873072e03
Christian Chesire <christianck.chesire@gmail.com>
2025-06-10 19:01:51 +0300

    Moved the javascript backend directory to the right folder.

--- 0x05-Node_JS_basic/0-console.js
+++
@@ -1,9 +0,0 @@
-/**
- * Function that displays a message to STDOUT
- * @param {string} message - message to be displayed
- */
-function displayMessage(message) {
-  console.log(message);
-}
-
-module.exports = displayMessage;
